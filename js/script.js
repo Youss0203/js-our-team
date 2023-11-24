ca4349a37ff01bb7c5d676f1fd43643d9dfd2fdf
Youss0203 <youssefhalibi003@gmail.com>
2023-11-24 15:07:04 +0100

    creato array di oggetti

---
+++ js/script.js
@@ -0,0 +1,41 @@
+/* 
+Wayne Barnett	Founder & CEO	wayne-barnett-founder-ceo.jpg
+Angela Caroll	Chief Editor	angela-caroll-chief-editor.jpg
+Walter Gordon	Office Manager	walter-gordon-office-manager.jpg
+Angela Lopez	Social Media Manager	angela-lopez-social-media-manager.jpg
+Scott Estrada	Developer	scott-estrada-developer.jpg
+Barbara Ramos	Graphic Designer	barbara-ramos-graphic-designer.jpg */
+
+
+const membri=[
+{
+    nome:"Wayne",
+    cognome:"Barnett",
+    ruolo:"Founder & CEO",
+},
+{
+    nome:"Angela",
+    cognome:"Caroll",
+    ruolo:"Chief Editor",
+},
+{
+    nome:"Walter",
+    cognome:"Gordon",
+    ruolo:"Office Manager",
+},
+{
+    nome:"Angela",
+    cognome:"Lopez",
+    ruolo:"Social Media Manager",
+},
+{
+    nome:"Scott",
+    cognome:"Estrada",
+    ruolo:"Developer",
+},
+{
+    nome:"Barbara",
+    cognome:"Ramos",
+    ruolo:"Graphic Designer"
+},
+]
